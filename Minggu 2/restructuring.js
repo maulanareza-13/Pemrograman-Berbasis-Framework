aa658c5f7c4a0d33c37cdd10bcea6970e50cd01b
rezabeker <rezabeker8@gmail.com>
2021-03-02 14:42:28 +0700

    update

---
+++ Minggu 2/restructuring.js
@@ -0,0 +1,8 @@
+var pemainSepakbola = {
+    nama : 'Salah',
+    height : '175',
+    output(){
+        alert(`Mr. ${this.name} is ${this.height} centimeter tall`);
+    }
+};
+pemainSepakbola.output();
